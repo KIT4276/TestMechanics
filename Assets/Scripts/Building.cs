3b83f20f3c5b15f0e973833fdd2f712d6dc85dc4
KIT4276 <kit311290@gmail.com>
2024-11-16 17:06:01 +0300

    color of Buildings

--- Assets/Scripts/Building.cs
+++ Assets/Scripts/Building.cs
@@ -2,9 +2,46 @@ using UnityEngine;
 
 public class Building : MonoBehaviour
 {
-    [SerializeField] private Vector2Int _cellSize = Vector2Int.one;
-    [SerializeField] private Color _color_1;
-    [SerializeField] private Color _color_2;
+    [SerializeField] private Vector2 _cellSize = Vector2Int.one;
+    [SerializeField] private Color _color;
+    [Space]
+    [SerializeField] private Material _okMaterial;
+    [SerializeField] private Material _noOkMaterial;
+    [SerializeField] private Material _flyingMaterial;
+    [SerializeField] private MeshRenderer[] _meshRenderer;
+
+    public void OkMAterial()
+    {
+        Debug.Log("OkMAterial");
+
+        foreach (var renderer in _meshRenderer)
+        {
+            renderer.material = _okMaterial;
+            Debug.Log(renderer.material.name);
+        }
+    }
+
+    public void NoOkMAterial()
+    {
+        Debug.Log("NoOkMAterial");
+        foreach (var renderer in _meshRenderer)
+        {
+            renderer.material = _noOkMaterial;
+            Debug.Log(renderer.material.name);
+        }
+    }
+
+    public void FlyingManerial()
+    {
+        Debug.Log("FlyingManerial");
+        foreach (var renderer in _meshRenderer)
+        {
+            renderer.material = _flyingMaterial;
+            Debug.Log(renderer.material.name);
+        }
+    }
+
+    public Vector2 CellSize { get => _cellSize;}
 
     private void OnDrawGizmosSelected()
     {
@@ -12,10 +49,7 @@ public class Building : MonoBehaviour
         {
             for (int y = 0; y < _cellSize.y; y++)
             {
-                if ((x + y) % 2 == 0)
-                    Gizmos.color = _color_1;
-                else
-                    Gizmos.color = _color_2;
+                Gizmos.color = _color;
 
                 Gizmos.DrawCube(transform.position + new Vector3(x, 0, y), new Vector3(1, 0.1f, 1));
             }
--- Assets/Scripts/BuildingsGrid.cs
+++ Assets/Scripts/BuildingsGrid.cs
@@ -4,38 +4,88 @@ public class BuildingsGrid : MonoBehaviour
 {
     [SerializeField] private Vector2Int _gridSize = new(10, 10);
 
-    private Building[,] _grid;
+    // private readonly Building[,] _grid;
     private Building _activeBuilding;
     private Camera _camera;
 
+    private Plane _groundPlane;
+    private Ray _ray;
+    private Vector3 _worldPosition;
+    private int _int_X;
+    private int _int_Z;
+    private bool _available;
+    private Material _material;
+
     private void Awake()
     {
-        _grid = new Building[_gridSize.x, _gridSize.y];
+        //_grid = new Building[_gridSize.x, _gridSize.y];
         _camera = Camera.main;
+        _groundPlane = new Plane(Vector3.up, Vector3.zero);
     }
 
     public void StartPlacingBuilding(Building buildingPrefab)
     {
         if (_activeBuilding != null)
-        { 
+        {
             Destroy(_activeBuilding.gameObject);
         }
 
-        _activeBuilding =  Instantiate(buildingPrefab);
+        _activeBuilding = Instantiate(buildingPrefab);
     }
 
     private void Update()
     {
-        if(_activeBuilding != null)
+        if (_activeBuilding != null)
         {
-            var groundPlane = new Plane(Vector3.up, Vector3.zero);
-            Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+            _ray = _camera.ScreenPointToRay(Input.mousePosition);
 
-            if(groundPlane.Raycast(ray, out float position)) 
-            { 
-                Vector3 worldPosition = ray.GetPoint(position);
-                _activeBuilding.transform.position = worldPosition;
+            if (_groundPlane.Raycast(_ray, out float position))
+            {
+                FindPosition(position);
+                IsAvailable();
+                PutABuilding();
             }
         }
+    }
+
+    private void PutABuilding()
+    {
+        _activeBuilding.transform.position = new Vector3(_int_X, 0, _int_Z);
+
+        if (_available)
+            _activeBuilding.FlyingManerial();
+        else
+            _activeBuilding.NoOkMAterial();
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (_available)
+            {
+                _activeBuilding.OkMAterial();
+                _activeBuilding = null;
+            }
+            else
+                Debug.Log("NONONO!");
+        }
+    }
+
+    private void FindPosition(float position)
+    {
+        _worldPosition = _ray.GetPoint(position);
+
+        _int_X = Mathf.RoundToInt(_worldPosition.x);
+        _int_Z = Mathf.RoundToInt(_worldPosition.z);
+    }
+
+    private void IsAvailable()
+    {
+        if (_int_X < (/*transform.position.x +*/ _activeBuilding.CellSize.x / 2)
+            || _int_X > (_gridSize.x - _activeBuilding.CellSize.x)
+            || _int_Z < (/*transform.position.z +*/ _activeBuilding.CellSize.y/2)
+            || _int_Z > (_gridSize.y - _activeBuilding.CellSize.y))
+            _available = false;
+        else
+            _available = true;
     }
 }
